6c67e7eb34a879507e24a0abe6834086ef86d530
erin.sheldon <erin.sheldon@359949b4-dc63-11dd-a550-c997509ed985>
2010-05-19 17:52:20 +0000

    added description

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ module1 = Extension('recfile._records', sources=sources)
 
 
 setup(name='recfile',
-      version='0.10',
-      description='This is a test',
+      description='A class for reading and writing files with fixed length records',
+      url='http://code.google.com/p/recfile/',
       packages=['recfile'],
       ext_modules=[module1],
       py_modules=['records'],
