713dafb9c0325292ae5bb493acd1f49ac93ab69c
erin.sheldon <erin.sheldon@359949b4-dc63-11dd-a550-c997509ed985>
2010-08-20 02:10:43 +0000

    cleaned up __init__

--- recfile/__init__.py
+++ recfile/__init__.py
@@ -1,15 +1,7 @@
-# This is a wrapper that returns an instance of the Records
-# class.  
-
-# this only works in 2.5
-#from .records import Records
-#from .Util import Open
-#from .Util import test
-
-from records import Records
 import Util
 from Util import Recfile
 from Util import Open
 from Util import test
 
+# use the same doc as the Util module
 __doc__=Util.__doc__
