1c1ac8ef7d391a9c2355beb58291ecdd4d9b40b3
erin.sheldon <erin.sheldon@359949b4-dc63-11dd-a550-c997509ed985>
2011-02-07 19:34:51 +0000

    use the dataview when sending to the c code for writing.  ensure native before sending

--- trunk/setup.py
+++ trunk/setup.py
@@ -11,25 +11,6 @@ sources=['records.cpp', 'records_wrap.cpp']
 sources = [os.path.join(subdir,f) for f in sources]
 module1 = Extension('recfile._records', sources=sources)
 
-crap='''
-# create the ups table
-pyvers='%s.%s' % sys.version_info[0:2]
-d1='lib/python%s/site-packages' % pyvers
-d2='lib64/python%s/site-packages' % pyvers
-
-if not os.path.exists('ups'):
-    os.mkdir('ups')
-tablefile=open('ups/recfile.table','w')
-tab="""
-setupOptional("python")
-setupOptional("numpy")
-envPrepend(PYTHONPATH,${PRODUCT_DIR}/%s)
-envPrepend(PYTHONPATH,${PRODUCT_DIR}/%s)
-""" % (d1,d2)
-tablefile.write(tab)
-tablefile.close()
-'''
-
 data_files=[]
 
 class AddUPS(Command):
